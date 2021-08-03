158eff3300e4c47315e03f0fff7eeb0019b5928f
Cainã Figueiredo <cainafigueiredo@poli.ufrj.br>
2021-08-03 10:22:44 -0300

    Float variables changed to double. Elapsed Time is now measured.

--- exercises/speedup-trapezoidal-rule/src/sequential.c
+++ exercises/speedup-trapezoidal-rule/src/sequential.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 #include <math.h>
+#include <sys/time.h>
 
-float f(float x) { /* Calcula f(x). */
-  float return_val;
-  return_val=exp(x);
-  return return_val;
+double f(double x) { /* Calcula f(x). */
+	double return_val;
+	return_val=exp(x);
+	return return_val;
 }
 
 int main(int argc, char *argv[]) { /* trapezio_seq.c */
-  float integral; /* integral armazena resultado final */
-  float a, b; /* a, b - limite esquerdo e direito da função */
-  int i,n; /* n - número de trapezóides */
-  float x, h; /* h - largura da base do trapezóide */
-  printf("Entre a, b, e n \n");
-  scanf("%f %f %d", &a, &b, &n);
-  h = (b-a)/n;
-  integral = (f(a) + f(b))/2.0;
-  x = a;
-  for (i = 1; i < n; i++) {
-    x += h;
-    integral += f(x);
-  }
-  integral *= h;
-  printf("Com n = %d trapezóides, a estimativa \n", n);
-  printf("da integral de %f até %f = %f\n", a, b, integral);
-  return(0);
+	double integral; /* integral armazena resultado final */
+	double a = 0.0, b = 1.0; /* a, b - limite esquerdo e direito da função */
+	long int i,n; /* n - número de trapezóides */
+	n = 100000000;
+	double x, h; /* h - largura da base do trapezóide */
+	struct timeval tempo_inicial, tempo_final, tempo_gasto;
+	//printf("Entre a, b, e n \n");
+	//scanf("%lf %lf %ld", &a, &b, &n);
+	h = (b-a)/n;
+	gettimeofday(&tempo_inicial, NULL);
+	integral = (f(a) + f(b))/2.0;
+	x = a;
+	for (i = 1; i < n; i++) {
+		x += h;
+		integral += f(x);
+	}
+	integral *= h;
+	gettimeofday(&tempo_final, NULL);
+	timersub(&tempo_final, &tempo_inicial, &tempo_gasto);
+	printf("Foram gastos %3.1f segundos\n", (double) tempo_gasto.tv_sec + (double) (tempo_gasto.tv_usec/1000000.0));
+	printf("Com n = %ld trapezóides, a estimativa \n", n);
+	printf("da integral de %lf até %lf = %lf \n", a, b, integral);
+	return(0);
 }
+
